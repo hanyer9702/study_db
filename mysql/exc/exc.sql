2ec1770db52d2b7f37ba2636ddd1a0ef513e7f3d
owner <hanyer9702@naver.com>
2022-01-24 11:52:33 +0900

    220124

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,21 @@
+use world;
+
+-- show tables;
+
+-- 테이블 생성
+create table member (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+-- 테이블 삭제
+
