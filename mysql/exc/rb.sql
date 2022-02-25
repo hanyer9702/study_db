6ae68818aad4468aea361a5d6e23723058fa2607
owner <hanyer9702@naver.com>
2022-02-25 11:10:01 +0900

    220225

--- mysql/exc/rb.sql
+++ mysql/exc/rb.sql
@@ -47,4 +47,441 @@ CREATE TABLE IF NOT EXISTS `shmlCategoryEntry` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS `rb`.`infrCodeGroup` (
+  `ifcgSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifcgSeqAnother` VARCHAR(50) NULL,
+  `ifcgName` VARCHAR(100) NOT NULL,
+  `ifcgNameEng` VARCHAR(100) NULL,
+  `ifcgUseNy` TINYINT NULL,
+  `ifcgOrder` TINYINT NULL,
+  `ifcgReferenceV1` VARCHAR(50) NULL,
+  `ifcgReferenceV2` VARCHAR(50) NULL,
+  `ifcgReferenceV3` VARCHAR(50) NULL,
+  `ifcgReferenceI1` INT NULL,
+  `ifcgReferenceI2` INT NULL,
+  `ifcgReferenceI3` INT NULL,
+  `ifcgDesc` VARCHAR(255) NULL,
+  `ifcgDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  PRIMARY KEY (`ifcgSeq`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrCode` (
+  `ifcdSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifcdSeqAnother` VARCHAR(50) NULL,
+  `ifcdName` VARCHAR(50) NOT NULL,
+  `ifcdNameEng` VARCHAR(50) NULL,
+  `ifcdNameLang1` VARCHAR(50) NULL,
+  `ifcdNameLang2` VARCHAR(50) NULL,
+  `ifcdNameLang3` VARCHAR(50) NULL,
+  `ifcdNameLang4` VARCHAR(50) NULL,
+  `ifcdNameLang5` VARCHAR(50) NULL,
+  `ifcdUseNy` TINYINT NULL,
+  `ifcdOrder` TINYINT NULL,
+  `ifcdReferenceV1` VARCHAR(50) NULL,
+  `ifcdReferenceV2` VARCHAR(50) NULL,
+  `ifcdReferenceV3` VARCHAR(50) NULL,
+  `ifcdReferenceI1` INT NULL,
+  `ifcdReferenceI2` INT NULL,
+  `ifcdReferenceI3` INT NULL,
+  `ifcdDesc` VARCHAR(255) NULL,
+  `ifcdDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifcgseq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifcdSeq`, `ifcgseq`),
+  INDEX `fk_infrCode_infrCodeGroup_idx` (`ifcgseq` ASC),
+  CONSTRAINT `fk_infrCode_infrCodeGroup`
+    FOREIGN KEY (`ifcgseq`)
+    REFERENCES `rb`.`infrCodeGroup` (`ifcgSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMember` (
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifmmAdminNy` TINYINT NULL,
+  `ifmmDormancyNy` TINYINT NULL,
+  `ifmmName` VARCHAR(50) NOT NULL,
+  `ifmmId` VARCHAR(50) NOT NULL,
+  `ifmmPassword` VARCHAR(100) NULL,
+  `ifmmPwdModDate` DATETIME NULL COMMENT '패스워드수정일',
+  `ifmmGenderCd` BIGINT NULL,
+  `ifmmDob` DATE NULL,
+  `ifmmSavedCd` BIGINT NULL COMMENT '회원정보저장기간',
+  `ifmmMarriageCd` BIGINT NULL,
+  `ifmmMarriageDate` DATE NULL,
+  `ifmmChildrenNum` TINYINT NULL,
+  `ifmmFavoriteColor` VARCHAR(50) NULL,
+  `ifmmRecommenderSeq` BIGINT NULL,
+  `ifmmEmailConsentNy` TINYINT NULL,
+  `ifmmSmsConsentNy` TINYINT NULL,
+  `ifmmPushConsentNy` TINYINT NULL,
+  `ifmmDesc` VARCHAR(255) NULL,
+  `ifmmDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  PRIMARY KEY (`ifmmSeq`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMemberEmail` (
+  `ifmeSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifmeDefaultNy` TINYINT NOT NULL,
+  `ifmeTypeCd` BIGINT NULL,
+  `ifmeEmailFull` VARCHAR(100) NULL,
+  `ifmeEmailAccount` VARCHAR(100) NULL,
+  `ifmeEmailDomain` VARCHAR(100) NULL,
+  `ifmeEmailDomainCd` BIGINT NULL,
+  `ifmeDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifmeSeq`),
+  INDEX `fk_infrMemberEmail_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrMemberEmail_infrMember1`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMemberPhone` (
+  `ifmpSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifmpDefaultNy` TINYINT NOT NULL,
+  `ifmpTypeCd` BIGINT NULL,
+  `ifmpDeviceCd` BIGINT NULL,
+  `ifmpTelecomCd` BIGINT NULL,
+  `ifmpNumber` VARCHAR(50) NULL,
+  `ifmpDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifmpSeq`),
+  INDEX `fk_infrMemberEmail_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrMemberEmail_infrMember10`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMemberAddressOnline` (
+  `ifaoSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifaoTypeCd` BIGINT NULL,
+  `ifaoDefaultNy` BIGINT NOT NULL,
+  `ifaoSnsTypeCd` BIGINT NULL,
+  `ifaoUrl` VARCHAR(100) NULL,
+  `ifaoTitle` VARCHAR(100) NULL,
+  `ifaoDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifaoSeq`),
+  INDEX `fk_infrMemberEmail_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrMemberEmail_infrMember100`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMemberAddress` (
+  `ifmaSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifmaDefaultNy` TINYINT NOT NULL,
+  `ifmaTypeCd` BIGINT NULL,
+  `ifmaTitle` VARCHAR(100) NULL,
+  `ifmaAddress1` VARCHAR(100) NULL,
+  `ifmaAddress2` VARCHAR(100) NULL,
+  `ifmaZipcode` VARCHAR(50) NULL,
+  `ifmaDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifmaSeq`),
+  INDEX `fk_infrMemberEmail_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrMemberEmail_infrMember11`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMemberJoinQna` (
+  `ifjqSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifjqQuestionCd` BIGINT NOT NULL,
+  `ifjqAnswer` VARCHAR(50) NOT NULL,
+  `ifjqDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifjqSeq`),
+  INDEX `fk_infrMemberEmail_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrMemberEmail_infrMember1100`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMemberHobby` (
+  `ifmhSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifmhHobbyCd` BIGINT NOT NULL,
+  `ifmhUseNy` TINYINT NULL,
+  `ifmhOrder` TINYINT NULL,
+  `ifmaDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifmhSeq`),
+  INDEX `fk_infrMemberEmail_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrMemberEmail_infrMember110`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrNationality` (
+  `ifnaSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifnaName` VARCHAR(50) NULL,
+  `ifnaNameKor` VARCHAR(50) NULL,
+  `ifnaNameEng` VARCHAR(50) NULL,
+  `ifnaCode2Char` CHAR(2) NULL,
+  `ifnaCode3Char` CHAR(3) NULL,
+  `ifnaUseNy` TINYINT NULL,
+  `ifnaOrder` TINYINT NULL,
+  `ifnaDesc` VARCHAR(255) NULL,
+  `ifnaDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  PRIMARY KEY (`ifnaSeq`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMemberNationality` (
+  `ifmnSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifmnDefaultNy` TINYINT NOT NULL,
+  `ifmnDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifnaSeq` BIGINT UNSIGNED NOT NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifmnSeq`),
+  INDEX `fk_infrMemberNationality_infrNationality1_idx` (`ifnaSeq` ASC),
+  INDEX `fk_infrMemberNationality_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrMemberNationality_infrNationality1`
+    FOREIGN KEY (`ifnaSeq`)
+    REFERENCES `rb`.`infrNationality` (`ifnaSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_infrMemberNationality_infrMember1`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrAuth` (
+  `ifatSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifatAdminNy` TINYINT NULL,
+  `ifatName` VARCHAR(50) NOT NULL,
+  `ifatNameEng` VARCHAR(50) NULL,
+  `ifatUseNy` TINYINT NULL,
+  `ifatOrder` TINYINT NULL,
+  `ifatDesc` VARCHAR(255) NULL,
+  `ifatDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  PRIMARY KEY (`ifatSeq`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrAuthMember` (
+  `ifamSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifamDefaultNy` TINYINT NULL,
+  `ifamUseNy` TINYINT NULL,
+  `ifamOrder` TINYINT NULL,
+  `ifamDesc` VARCHAR(255) NULL,
+  `ifamDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifatSeq` BIGINT UNSIGNED NOT NULL,
+  `ifmmSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifamSeq`),
+  INDEX `fk_infrAuthMember_infrAuth1_idx` (`ifatSeq` ASC),
+  INDEX `fk_infrAuthMember_infrMember1_idx` (`ifmmSeq` ASC),
+  CONSTRAINT `fk_infrAuthMember_infrAuth1`
+    FOREIGN KEY (`ifatSeq`)
+    REFERENCES `rb`.`infrAuth` (`ifatSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_infrAuthMember_infrMember1`
+    FOREIGN KEY (`ifmmSeq`)
+    REFERENCES `rb`.`infrMember` (`ifmmSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrMenu` (
+  `ifmuSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifmuAdminNy` TINYINT NULL,
+  `ifmuName` VARCHAR(50) NOT NULL,
+  `ifmuNameEng` VARCHAR(50) NULL,
+  `ifmuUrl` VARCHAR(100) NULL,
+  `ifmuParents` BIGINT NULL,
+  `ifmuDepth` TINYINT NULL COMMENT '단계정보',
+  `ifmuUseNy` TINYINT NULL,
+  `ifmuOrder` TINYINT NULL,
+  `ifmuDesc` VARCHAR(255) NULL,
+  `ifmuDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  PRIMARY KEY (`ifmuSeq`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rb`.`infrAuthMenu` (
+  `ifauSeq` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ifauRoleCd` BIGINT NULL COMMENT '관리, 조회\n목록, 등록, 수정, 삭제, 조회',
+  `ifauUseNy` TINYINT NULL,
+  `ifauOrder` TINYINT NULL,
+  `ifauDesc` VARCHAR(255) NULL,
+  `ifauDelNy` TINYINT NOT NULL,
+  `regIp` VARCHAR(100) NULL,
+  `regSeq` BIGINT NULL,
+  `regDevice` TINYINT NULL,
+  `regDateTime` DATETIME NULL,
+  `regDateTimeSvr` DATETIME NULL,
+  `modIp` VARCHAR(100) NULL,
+  `modSeq` BIGINT(11) NULL,
+  `modDevice` TINYINT NULL,
+  `modDateTime` DATETIME NULL,
+  `modDateTimeSvr` DATETIME NULL,
+  `ifatSeq` BIGINT UNSIGNED NOT NULL,
+  `ifmuSeq` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`ifauSeq`),
+  INDEX `fk_infrAuthMenu_infrAuth1_idx` (`ifatSeq` ASC),
+  INDEX `fk_infrAuthMenu_infrMenu1_idx` (`ifmuSeq` ASC),
+  CONSTRAINT `fk_infrAuthMenu_infrAuth1`
+    FOREIGN KEY (`ifatSeq`)
+    REFERENCES `rb`.`infrAuth` (`ifatSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_infrAuthMenu_infrMenu1`
+    FOREIGN KEY (`ifmuSeq`)
+    REFERENCES `rb`.`infrMenu` (`ifmuSeq`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 -- \func now()
